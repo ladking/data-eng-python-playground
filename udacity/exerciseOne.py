f9d07106ee27045b70b4a79d5eb2f710e8d897ea
ladi <ladigrillo48@gmail.com>
2023-12-13 04:05:51 +0100

    update

---
+++ udacity/exerciseOne.py
@@ -0,0 +1,39 @@
+
+songplays = {
+    "id":"int",
+    "songplayid":"int",
+    "starttime":"DATE",
+    "userid":"int",
+    "level":"string",
+    "song_id":"int",
+    "artist_id":"int",
+    "session_id":"int",
+    "location": "string",
+    "user_agent":"string"
+}
+
+songsModel = {
+    "id":"int",
+    "title":"string",
+    "artist_id":"int",
+    "year":"string",
+    "duration":"string"
+    }
+
+artistModel={
+    "id":"int",
+    "name":"string",
+    "location":"string",
+    "latitude":"string",
+    "longitude":"string",
+}
+
+time ={
+    "start_time":"",
+    "hour":"",
+    "day":"",
+    "week":"",
+    "month":"",
+    "year":"",
+    "weekday":""
+}
